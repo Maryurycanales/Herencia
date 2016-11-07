f4dfb5c19c13fb1ecc7e27c5916f8ac27b484c2e
Maryurycanales <maryu_canales@hotmail.com>
2016-11-07 09:19:36 -0600

    Add files via upload

---
+++ EjerciciosHerencia-master/src/Asteroide.java
@@ -0,0 +1,15 @@
+
+public class Asteroide extends CuerpoEstelar  {
+
+	int direccion;
+	
+	Asteroide(int posicion, int masa,int direccion)
+	{
+		super(posicion,masa);
+		this.direccion=direccion;
+	
+	}
+	
+}
+
+
